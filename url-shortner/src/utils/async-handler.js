4956df23dd5f05a05411cbadff56c3a63cc4a0c5
harshad diwate <harshaddiwate85@gmail.com>
2026-01-11 11:12:39 +0530

    feat: add dotenv dependency and implement async error handler utility

---
+++ url-shortner/src/utils/async-handler.js
@@ -0,0 +1,16 @@
+
+export  const async_handler =   (request_handler)=>async (req,res,next)=>{
+  
+    try{
+     await request_handler(req,res,next)
+        
+    }
+    catch(error)
+    {
+        console.log(`ERROR : ${error}`);
+res.status(error.statusCode ||500).json({
+    sucess:false,
+    message:error.message
+})        
+    }
+ }
