f89346637e36ff6e5382b9bc38fa148de3959690
harshad diwate <harshaddiwate85@gmail.com>
2026-02-06 00:07:04 +0530

    feat: Add initial HTML, CSS, and JavaScript files for GSAP animation

---
+++ GSAP/basics/index.js
@@ -0,0 +1,16 @@
+ gsap.to(".box", {
+      x: 300,
+    borderRadius:"50%",
+    backgroundColor:"cyan",
+      scale:2,
+      duration: 4,
+      rotation: 360
+    });
+ gsap.from(".box", {
+      x: 300,
+    backgroundColor:"cyan",
+      scale:2,
+      duration: 4,
+      rotation: 360
+    });
+
