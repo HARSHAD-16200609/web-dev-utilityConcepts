74595e61ffdee46b3eab08e99bd39a587d7eedc8
harshad diwate <harshaddiwate85@gmail.com>
2026-02-06 00:46:13 +0530

    refactor: Comment out unused GSAP animations and clean up HTML structure

--- GSAP/basics/index.js
+++ GSAP/basics/index.js
@@ -1,16 +1,26 @@
- gsap.to(".box", {
-      x: 300,
-    borderRadius:"50%",
-    backgroundColor:"cyan",
-      scale:2,
-      duration: 4,
-      rotation: 360
-    });
- gsap.from(".box", {
-      x: 300,
-    backgroundColor:"cyan",
-      scale:2,
-      duration: 4,
-      rotation: 360
-    });
+//  gsap.to(".box", {
+//       x: 300,
+//     borderRadius:"50%",
+//     backgroundColor:"cyan",
+//       scale:2,
+//       duration: 4,
+//       rotation: 360
+//     });
 
+
+//  gsap.from(".box", {
+//       x: 300,
+//     backgroundColor:"cyan",
+//       scale:2,
+//       duration: 4,
+//       rotation: 360
+//     });
+
+
+gsap.from("h1",{
+  y:40,
+opacity:0,
+color:"crimson",
+duration:2,
+stagger:0.2 // if - sign used then stagger reversed direction
+})
