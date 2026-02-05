9821982d4ef8cfd32bf38592f5443e8960a787d6
harshad diwate <harshaddiwate85@gmail.com>
2026-02-06 01:01:35 +0530

    feat:learn repeat and yoyo

--- GSAP/basics/index.js
+++ GSAP/basics/index.js
@@ -1,11 +1,11 @@
-//  gsap.to(".box", {
-//       x: 300,
-//     borderRadius:"50%",
-//     backgroundColor:"cyan",
-//       scale:2,
-//       duration: 4,
-//       rotation: 360
-//     });
+ gsap.to(".box", {
+      x: 300,
+      duration: 4,
+      rotation: 360,
+      repeat:3, // -1 makes it infinite
+      yoyo:true // this makes the animation of form   gsap.to then gsap.from 
+
+    });
 
 
 //  gsap.from(".box", {
@@ -17,10 +17,10 @@
 //     });
 
 
-gsap.from("h1",{
-  y:40,
-opacity:0,
-color:"crimson",
-duration:2,
-stagger:0.2 // if - sign used then stagger reversed direction
-})
+// gsap.from("h1",{
+//   y:40,
+// opacity:0,
+// color:"crimson",
+// duration:2,
+// stagger:0.2 // if - sign used then stagger reversed direction
+// })
