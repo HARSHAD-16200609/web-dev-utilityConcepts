2087ad2ae7d236773ed00805142cc53c65461224
harshad diwate <harshaddiwate85@gmail.com>
2026-02-07 00:41:19 +0530

    feat: Add initial HTML, CSS, and JavaScript files for GSAP ScrollTrigger animations

---
+++ GSAP/ScrollTrigger/index.js
@@ -0,0 +1,17 @@
+gsap.from(" #page1 .box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360,
+})
+
+ // basic scroll trigger 
+
+gsap.from(" #page2 .box",{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:360,
+    scrollTrigger:" #page2 .box",
+    backgroundColor:"red"
+})
