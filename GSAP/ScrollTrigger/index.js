c9745a77fe5f9bb8900ef213e200f424d28bd377
harshad diwate <harshaddiwate85@gmail.com>
2026-02-07 00:49:47 +0530

    feat: Update HTML structure and enhance GSAP animation with scroll trigger settings

--- GSAP/ScrollTrigger/index.js
+++ GSAP/ScrollTrigger/index.js
@@ -7,11 +7,28 @@ gsap.from(" #page1 .box",{
 
  // basic scroll trigger 
 
+// gsap.from(" #page2 .box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:" #page2 .box",
+//     backgroundColor:"red"
+// })
+
 gsap.from(" #page2 .box",{
     scale:0,
     delay:1,
     duration:2,
     rotate:360,
-    scrollTrigger:" #page2 .box",
-    backgroundColor:"red"
+    scrollTrigger:{
+        trigger:"#page2 .box",
+        scroller:"body", 
+        markers:true,
+        start:"top 60%" // when this matche swith the stat of object animation is triggered 
+    },
+    
 })
+
+
+
