2edc989f0ec578c8a31a7c0330b9555f1410391e
harshad diwate <harshaddiwate85@gmail.com>
2026-02-10 23:51:51 +0530

    feat: Enhance text animation by splitting characters into left and right spans for improved GSAP effects

--- GSAP/textAnimation/index.js
+++ GSAP/textAnimation/index.js
@@ -6,22 +6,31 @@ var h1text=h1.textContent;
 var text = h1text.split("");
 var clutter ="";
 
-text.forEach((char)=>{
+text.forEach((char,idx)=>{
 
-    clutter+=`<span>${char}</span>`;
-    
-})
+    if(idx < text.length/2){
+    clutter+=`<span class="left">${char}</span>`;
 
+    }else{
+    clutter+=`<span class="right">${char}</span>`;
+    }
+})    
 h1.innerHTML=clutter;
 }
 
 breakText();
 
-gsap.from("h1 span",{
+gsap.from("h1 .left",{
     y:200,
     stagger:0.3,
-    duration:1,
-    ease:"power3.out",
+    duration:0.5,
+    delay:0.5,
+    opacity:0
+})
+gsap.from("h1 .right",{
+    y:200,
+    stagger:-0.3,
+    duration:0.5,
     delay:0.5,
     opacity:0
 })
